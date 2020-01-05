bcf11c3e16701f93df888902528faa616b808394
snowadmirer <pengjiade@gmail.com>
2020-01-06 07:35:29 +0800

    Update process.py

--- process.py
+++ process.py
@@ -1,5 +1,8 @@
-import numpy
+#coding=utf-8
+from PIL import Image, ImageFont, ImageDraw, ImageFilter
+import numpy as np
 import cv2
+import codecs
 import random
 
 def resize_with_pad(image, new_size, pad_val = 255):
@@ -39,3 +42,56 @@ def get_image_paths(image_dir, post_fixes = ['.jpg', '.png', '.jpeg', '.bmp', '.
         if os.path.isdir(filepath):
             img_paths.extend(get_image_paths(filepath, post_fixes))
     return img_paths
+
+def draw_text(font, image, pos, word, fill):
+    pil_image = Image.fromarray(image.astype('uint8')).convert('RGB')
+    draw = ImageDraw.Draw(pil_image)
+    draw.text(pos, word, font=font, fill=fill)
+    return np.array(pil_image, dtype=np.uint8)
+
+def draw_outline_text(font, image, pos, word, fill, outlinefill, offset):
+    pil_image = Image.fromarray(image.astype('uint8')).convert('RGB')
+    draw = ImageDraw.Draw(pil_image)
+    x, y = pos
+    draw.text((x-offset, y), word, font=font, fill=outlinefill)
+    draw.text((x, y-offset), word, font=font, fill=outlinefill)
+    draw.text((x+offset, y), word, font=font, fill=outlinefill)
+    draw.text((x, y+offset), word, font=font, fill=outlinefill)
+
+    draw.text((x-offset, y-offset), word, font=font, fill=outlinefill)
+    draw.text((x-offset, y+offset), word, font=font, fill=outlinefill)
+    draw.text((x+offset, y+offset), word, font=font, fill=outlinefill)
+    draw.text((x+offset, y-offset), word, font=font, fill=outlinefill)
+
+    draw.text(pos, word, font=font, fill=fill)
+    return np.array(pil_image, dtype=np.uint8)
+
+def get_word_box(font, word):
+    fontsize = font.size
+    image = Image.new('L', (fontsize * 2, fontsize* 3), 0)
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), word, font=font, fill=(255))
+    array = np.array(image, dtype=np.uint8)
+    row_sum = np.sum(array, axis=0)
+    col_sum = np.sum(array, axis=1)
+    x1 = 0
+    x2 = len(row_sum)
+    y1 = 0
+    y2 = len(col_sum)
+    for x in range(0, len(row_sum)):
+        if row_sum[x] > 0:
+            x1 = x
+            break
+    for x in range(0, len(row_sum)):
+        if row_sum[len(row_sum) - 1 - x] > 0:
+            x2 = len(row_sum) - x
+            break 
+    for y in range(0, len(col_sum)):
+        if col_sum[y] > 0:
+            y1 = y
+            break
+    for y in range(0, len(col_sum)):
+        if col_sum[len(col_sum) - 1 - y] > 0:
+            y2 = len(col_sum) - y
+            break
+    return (x1, y1, x2, y2)
