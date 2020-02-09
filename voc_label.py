5bafb04eed052a64ebbcbd3e0f2223ab0ad265e9
snowadmirer <pengjiade@gmail.com>
2020-02-10 01:47:24 +0800

    Update voc_label.py

--- voc_label.py
+++ voc_label.py
@@ -20,3 +20,35 @@ def read_voc_label(label_file):
         labels.append((xmin, ymin, xmax, ymax, name))
 
     return labels
+
+def make_voc_label(filepath, height, width, bboxes, savepath):
+    filename = os.path.basename(filepath)
+    folder = os.path.dirname(filepath.replace('/apsarapangu/disk2/jiade.pjd/data/ingredient/classify_label', ''))
+    E = objectify.ElementMaker(annotate=False)
+    anno_tree = E.annotation(
+                E.folder(folder),
+                E.filename(filename),
+                E.size(
+                    E.width(800),
+                    E.height(600),
+                    E.depth(3)
+                ),
+
+                )
+    for bbox in bboxes:
+        xmin, ymin, xmax, ymax = bbox[:4]
+        class_name = bbox[4]
+        obj = E.object(
+            E.name(class_name),
+            E.pose('Unspecified'),
+            E.difficult('0'),
+            E.bndbox(
+                E.xmin(str(xmin)),
+                E.ymin(str(ymin)),
+                E.xmax(str(xmax)),
+                E.ymax(str(ymax)),
+            ),
+        )
+        anno_tree.append(obj)
+
+    etree.ElementTree(anno_tree).write(savepath, pretty_print=True)
